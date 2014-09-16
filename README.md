4f2b64bdf12c45c6f54481396afe55be7a23be6a
Trevor Livingston <trlivingston@paypal.com>
2014-09-16 10:18:22 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# enjoi
+
+Converts a JSON schema to a Joi schema.
+
+```javascript
+var enjoi = require('enjoi');
+
+var schema = enjoi({
+    "title": "Example Schema",
+    "type": "object",
+    "properties": {
+        "firstName": {
+            "type": "string"
+        },
+        "lastName": {
+            "type": "string"
+        },
+        "age": {
+            "description": "Age in years",
+            "type": "integer",
+            "minimum": 0
+        }
+    },
+    "required": ["firstName", "lastName"]
+});
+```
+
+Will now be a Joi schema.
---
+++ test/test-enjoi.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var test = require('tape'),
+    enjoi = require('../lib/enjoi'),
+    joi = require('joi');
+
+test('enjoi', function (t) {
+
+    t.test('valid schema', function (t) {
+        t.plan(1);
+
+        var schema = enjoi({
+        	"title": "Example Schema",
+        	"type": "object",
+        	"properties": {
+        		"firstName": {
+        			"type": "string"
+        		},
+        		"lastName": {
+        			"type": "string"
+        		},
+        		"age": {
+        			"description": "Age in years",
+        			"type": "integer",
+        			"minimum": 0
+        		}
+        	},
+        	"required": ["firstName", "lastName"]
+        });
+
+        joi.validate({firstName: "John", lastName: "Doe", age: 45}, schema, function (error, value) {
+            t.ok(!error, 'no error.');
+        });
+    });
+
+    t.test('valid with ref', function (t) {
+        t.plan(1);
+
+        var schema = enjoi({
+            "title": "Example Schema",
+            "type": "object",
+            "properties": {
+                "name": {
+                    "$ref": "#/definitions/name"
+                }
+            },
+            "definitions": {
+                "name": {
+                    "type": "string"
+                }
+            }
+        });
+
+        joi.validate({name: 'Joe'}, schema,  function (error, value) {
+            t.ok(!error, 'no error.');
+            console.log(error);
+        });
+    });
+
+});
