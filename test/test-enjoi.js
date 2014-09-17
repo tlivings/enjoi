0b6cf6e649ddacaf111d680ad07849857fb0ce9b
Trevor Livingston <trlivingston@paypal.com>
2014-09-16 17:14:45 -0700

    adding more coverage

--- test/test-enjoi.js
+++ test/test-enjoi.js
@@ -6,47 +6,86 @@ var test = require('tape'),
 
 test('enjoi', function (t) {
 
-    t.test('valid schema', function (t) {
+    t.test('valid', function (t) {
         t.plan(1);
 
         var schema = enjoi({
-        	"title": "Example Schema",
-        	"type": "object",
-        	"properties": {
-        		"firstName": {
-        			"type": "string"
+        	'title': 'Example Schema',
+        	'type': 'object',
+        	'properties': {
+        		'firstName': {
+        			'type': 'string'
         		},
-        		"lastName": {
-        			"type": "string"
+        		'lastName': {
+        			'type': 'string'
         		},
-        		"age": {
-        			"description": "Age in years",
-        			"type": "integer",
-        			"minimum": 0
+                'tags': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'string'
+                    }
+                },
+        		'age': {
+        			'description': 'Age in years',
+        			'type': 'integer',
+        			'minimum': 0
         		}
         	},
-        	"required": ["firstName", "lastName"]
+        	'required': ['firstName', 'lastName']
         });
 
-        joi.validate({firstName: "John", lastName: "Doe", age: 45}, schema, function (error, value) {
+        joi.validate({firstName: 'John', lastName: 'Doe', age: 45, tags: ['man', 'human']}, schema, function (error, value) {
             t.ok(!error, 'no error.');
         });
     });
 
-    t.test('valid with ref', function (t) {
+    t.test('invalid', function (t) {
         t.plan(1);
 
         var schema = enjoi({
-            "title": "Example Schema",
-            "type": "object",
-            "properties": {
-                "name": {
-                    "$ref": "#/definitions/name"
+            'title': 'Example Schema',
+            'type': 'object',
+            'properties': {
+                'firstName': {
+                    'type': 'string'
+                },
+                'lastName': {
+                    'type': 'string'
+                },
+                'tags': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'string'
+                    }
+                },
+                'age': {
+                    'description': 'Age in years',
+                    'type': 'integer',
+                    'minimum': 0
                 }
             },
-            "definitions": {
-                "name": {
-                    "type": "string"
+            'required': ['firstName', 'lastName']
+        });
+
+        joi.validate({firstName: 'John', lastName: 'Doe', age: 45, tags: [1, 'human']}, schema, function (error, value) {
+            t.ok(error, 'no error.');
+        });
+    });
+
+    t.test('with ref', function (t) {
+        t.plan(1);
+
+        var schema = enjoi({
+            'title': 'Example Schema',
+            'type': 'object',
+            'properties': {
+                'name': {
+                    '$ref': '#/definitions/name'
+                }
+            },
+            'definitions': {
+                'name': {
+                    'type': 'string'
                 }
             }
         });
@@ -56,26 +95,45 @@ test('enjoi', function (t) {
         });
     });
 
-    t.test('valid with external ref', function (t) {
+    t.test('with external ref', function (t) {
         t.plan(1);
 
         var schema = enjoi({
-            "title": "Example Schema",
-            "type": "object",
-            "properties": {
-                "name": {
-                    "$ref": "definitions#/name"
+            'title': 'Example Schema',
+            'type': 'object',
+            'properties': {
+                'name': {
+                    '$ref': 'definitions#/name'
                 }
             }
         }, {
-            "definitions": {
-                "name": {
-                    "type": "string"
+            'definitions': {
+                'name': {
+                    'type': 'string'
                 }
             }
         });
 
         joi.validate({name: 'Joe'}, schema,  function (error, value) {
+            t.ok(!error, 'no error.');
+        });
+    });
+
+});
+
+test('types', function (t) {
+
+    t.test('numbers', function (t) {
+        t.plan(1);
+
+        var schema = enjoi({
+            'type': 'array',
+            'items': {
+                'type': 'number'
+            }
+        });
+
+        joi.validate([1, 2], schema, function (error, value) {
             t.ok(!error, 'no error.');
         });
     });
