ae564917db976c86a6a26a59c79bf30ea9cda002
Vadzim Khomchyk <homchik@gmail.com>
2015-02-14 00:35:30 +0300

    is-my-json-valid benchmark

--- test/bench/run.js
+++ test/bench/run.js
@@ -3,9 +3,9 @@
 var Spawn = require('child_process').spawn;
 var Path = require('path');
 
-var enjoi, tv4, themis;
+var enjoi, tv4, themis, imjv;
 
-console.log('tv4 vs themis vs joi benchmark:\n');
+console.log('tv4 vs themis vs is-my-json-valid vs joi benchmark:\n');
 
 tv4 = Spawn('node', [Path.resolve(__dirname, 'tv4.js')]);
 
@@ -17,13 +17,18 @@ tv4.once('close', function () {
     themis.stdout.pipe(process.stdout);
 
     themis.once('close', function () {
-        enjoi = Spawn('node', [Path.resolve(__dirname, 'enjoi.js')]);
+        imjv = Spawn('node', [Path.resolve(__dirname, 'imjv.js')]);
 
-        enjoi.stdout.pipe(process.stdout);
+        imjv.stdout.pipe(process.stdout);
 
-        enjoi.once('close', function () {
-            process.exit(0);
+        imjv.once('close', function () {
+            enjoi = Spawn('node', [Path.resolve(__dirname, 'enjoi.js')]);
+
+            enjoi.stdout.pipe(process.stdout);
+
+            enjoi.once('close', function () {
+                process.exit(0);
+            });
         });
     });
-
 });
