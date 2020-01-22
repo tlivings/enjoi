c18401b9adbfa5cc62ccbebe3df3bf9cde4c9543
Dmytro Semenov <dimichsdg@yahoo.com>
2020-01-22 13:07:03 -0800

    Should support default false and 0 values (#83)
    Looks good to me. Thanks for fix!

--- test/test-directives.js
+++ test/test-directives.js
@@ -181,7 +181,7 @@ Test('directives', function (t) {
     });
 
     t.test('default values', function (t) {
-        t.plan(2);
+        t.plan(4);
 
         const schema = {
             type: 'object',
@@ -193,6 +193,18 @@ Test('directives', function (t) {
                 locale: {
                     type: 'string',
                     default: 'en-US'
+                },
+                isSubscribed: {
+                    type: 'boolean',
+                    default: false
+                },
+                posts: {
+                    type: 'number',
+                    default: 0
+                },
+                empty: {
+                    type: 'string',
+                    default: ''
                 }
             },
             required: ['user']
@@ -201,6 +213,8 @@ Test('directives', function (t) {
         Enjoi.schema(schema).validate({ user: 'test@domain.tld' }, function (error, value) {
             t.ok(!error, 'error');
             t.equal(value.locale, 'en-US');
+            t.equal(value.isSubscribed, false);
+            t.equal(value.posts, 0);
         });
     });
 
